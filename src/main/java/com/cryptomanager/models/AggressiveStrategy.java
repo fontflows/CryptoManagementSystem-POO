d08d05b92b473bc2918335db7e00b3542ede85d3
alvaro_rc <alvarocampioni7@gmail.com>
2024-10-23 19:31:42 -0300

    #6 Implementa interface InvestmentStrategy
    Implementa padrão de projeto Strategy para diferentes estratégias de investimento.

---
+++ src/main/java/com/cryptomanager/models/AggressiveStrategy.java
@@ -0,0 +1,41 @@
+package com.cryptomanager.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class AggressiveStrategy implements InvestmentStrategy{
+    private List<CryptoCurrency> aggressiveCryptos;
+
+    public AggressiveStrategy(List<CryptoCurrency> aggressiveCryptos) {
+        this.aggressiveCryptos = aggressiveCryptos != null ? aggressiveCryptos : new ArrayList<>();
+    }
+
+    @Override
+    public void setCryptos(List<CryptoCurrency> aggressiveCryptos) {
+        this.aggressiveCryptos = aggressiveCryptos;
+    }
+
+    @Override
+    public List<CryptoCurrency> getCryptos() {
+        return aggressiveCryptos;
+    }
+
+    @Override
+    public void addCryptoCurrency(CryptoCurrency crypto) {
+        if(crypto == null) { throw new IllegalArgumentException("Crypto não pode ser nula"); }
+        aggressiveCryptos.add(crypto);
+    }
+
+    @Override
+    public void removeCryptoCurrency(CryptoCurrency crypto) {
+        if(crypto == null) { throw new IllegalArgumentException("Crypto não pode ser nula"); }
+        aggressiveCryptos.remove(crypto);
+    }
+
+    @Override
+    public CryptoCurrency getRandomCrypto() {
+        Random rng = new Random();
+        return aggressiveCryptos.get(rng.nextInt(aggressiveCryptos.size()));
+    }
+}
