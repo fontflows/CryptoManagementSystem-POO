7bc40886814cbb4183e2545c9277c34cd1b5ff86
CN-2104 <chrisp.nantes@gmail.com>
2024-10-31 21:55:31 -0300

    Update ReportController.java
    Gera a partir do userid e portifolio id

--- src/main/java/com/cryptomanager/controllers/ReportController.java
+++ src/main/java/com/cryptomanager/controllers/ReportController.java
@@ -1,6 +1,7 @@
 package com.cryptomanager.controllers;
 
 import com.cryptomanager.models.Portfolio;
+import com.cryptomanager.repositories.PortfolioRepository;
 import com.cryptomanager.services.InvestmentReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,19 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/report")
 public class ReportController{
     private final InvestmentReportService investmentReportService;
-
+    private final PortfolioRepository portfolioRepository;
     @Autowired
-    public ReportController(InvestmentReportService investmentReportService) {
+    public ReportController(InvestmentReportService investmentReportService, PortfolioRepository portfolioRepository) {
         this.investmentReportService = investmentReportService;
+        this.portfolioRepository = portfolioRepository;
     }
 
     @PostMapping("/create-portifolio-report")
-    public ResponseEntity<String> CreatePortifolioRepository(@RequestBody Portfolio portfolio) {
+    public ResponseEntity<String> CreatePortifolioRepository(@RequestBody String portfolioid,@RequestBody String userid) {
+        Portfolio portfolio = portfolioRepository.loadPortfolioByUserIdAndPortfolioId(portfolioid,userid);
         investmentReportService.CreatePortifolioRepository(portfolio);
         return ResponseEntity.ok("Portfólio adicionado ou atualizado com sucesso!");
     }
     @PostMapping("/create-projected-portifolio-report")
-    public ResponseEntity<String> CreateProjectedPortifolioRepository(@RequestBody Portfolio portfolio,@RequestBody int meses) {
+    public ResponseEntity<String> CreateProjectedPortifolioRepository(@RequestBody String portfolioid,@RequestBody String userid,@RequestBody int meses) {
+        Portfolio portfolio = portfolioRepository.loadPortfolioByUserIdAndPortfolioId(portfolioid,userid);
         investmentReportService.CreateProjectedPortifolioRepository(portfolio,meses);
         return ResponseEntity.ok("Portfólio adicionado ou atualizado com sucesso!");
 
