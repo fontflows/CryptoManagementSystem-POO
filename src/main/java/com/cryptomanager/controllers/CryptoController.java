1e43743bd16fee1af81af1b719c285c67205e051
alvaro_rc <alvarocampioni7@gmail.com>
2024-11-12 16:28:00 -0300

    Padronização do CryptoController
    -Padronizacao do CryptoController:
    
    -Objetos não sao instanciados dentro do controller(Crypto)
    
    -Todas as excecoes conhecidas sao tratadas no service e enviadas ao controller como excecao personalizada: CryptoServiceException

--- src/main/java/com/cryptomanager/controllers/CryptoController.java
+++ src/main/java/com/cryptomanager/controllers/CryptoController.java
@@ -1,7 +1,10 @@
 package com.cryptomanager.controllers;
 
+import com.cryptomanager.exceptions.CryptoServiceException;
 import com.cryptomanager.models.CryptoCurrency;
 import com.cryptomanager.services.CryptoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,38 +22,50 @@ public class CryptoController {
     }
 
     @GetMapping
-    public List<CryptoCurrency> getAllCryptos() {
-        return cryptoService.getAllCryptos();
+    public ResponseEntity<?> getAllCryptos() {
+        try {
+            return ResponseEntity.ok(cryptoService.getAllCryptos());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @GetMapping("/search-by-name")
-    public CryptoCurrency getCryptoByName(String name) {
-        return cryptoService.getCryptoByName(name);
+    public ResponseEntity<?> getCryptoByName(String cryptoName) {
+        try {
+            return ResponseEntity.ok(cryptoService.getCryptoByName(cryptoName));
+        } catch (CryptoServiceException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @PostMapping("/add")
-    public String addCrypto(@RequestBody CryptoCurrency crypto) {
+    public ResponseEntity<String> addCrypto(@RequestParam String cryptoName, @RequestParam double price, @RequestParam double growthRate, @RequestParam double marketCap, @RequestParam double volume24h, @RequestParam int riskFactor) {
         try {
-            cryptoService.addCrypto(crypto);
-            return "Criptomoeda adicionada com sucesso!";
-        } catch (Exception e) {
-            return e.getMessage();
+            cryptoService.addCrypto(cryptoName, price, growthRate, marketCap, volume24h, riskFactor);
+            return ResponseEntity.ok("Criptomoeda adicionada com sucesso!");
+        } catch (CryptoServiceException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
     }
 
     @DeleteMapping("/delete")
-    public String deleteCrypto(@RequestParam String name) {
+    public ResponseEntity<String> deleteCrypto(@RequestParam String cryptoName) {
         try {
-            cryptoService.deleteCryptoByName(name);
-            return "Criptomoeda removida com sucesso!";
-        } catch (Exception e) {
-            return e.getMessage();
+            cryptoService.deleteCryptoByName(cryptoName);
+            return ResponseEntity.ok("Criptomoeda removida com sucesso!");
+        } catch (CryptoServiceException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 
     @PostMapping("/edit")
-    public String updateCrypto(@RequestBody CryptoCurrency crypto) {
-        cryptoService.updateCrypto(crypto);
-        return "Criptomoeda editada com sucesso!";
+    public ResponseEntity<String> updateCrypto(@RequestBody CryptoCurrency crypto) {
+        try {
+            cryptoService.updateCrypto(crypto);
+            return ResponseEntity.ok("Criptomoeda atualizada com sucesso!");
+        } catch (CryptoServiceException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
