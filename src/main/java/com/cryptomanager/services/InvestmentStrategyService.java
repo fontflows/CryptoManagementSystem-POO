de6c984548dbe72df994e47ac0c66683e4f4d916
alvaro_rc <alvarocampioni7@gmail.com>
2024-11-26 15:34:42 -0300

    InvestmentStrategyService javadoc

--- src/main/java/com/cryptomanager/services/InvestmentStrategyService.java
+++ src/main/java/com/cryptomanager/services/InvestmentStrategyService.java
@@ -9,16 +9,24 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
+/** Classe responsável pelos métodos Service das estratégias de investimento.*/
 @Service
 public class InvestmentStrategyService {
 
     private static CryptoRepository cryptoRepository;
 
+    /** Constructor InvestmentStrategyService
+     * @param cryptoRepository Instância que conecta o Service à classe que manipula os dados das CryptoCurrencies no arquivo.
+     */
     @Autowired
     public InvestmentStrategyService(CryptoRepository cryptoRepository) {
         InvestmentStrategyService.cryptoRepository = cryptoRepository;
     }
 
+    /** Atualiza a lista de CryptoCurrencies de uma dada estratégia de investimento baseado no fator de risco de cada moeda.
+     * @param investmentStrategy Estratégia de investimento cuja lista de CryptoCurrencies será atualizada.
+     * @throws IOException Caso ocorra erros na leitura das CryptoCurrencies no arquivo.
+     */
     public static void updateInvestmentStrategyList(InvestmentStrategy investmentStrategy) throws IOException {
         investmentStrategy.getSuggestedCryptos().clear();
         List<CryptoCurrency> cryptos = cryptoRepository.loadCryptos();
@@ -29,6 +37,10 @@ public class InvestmentStrategyService {
         }
     }
 
+    /** Obtém uma CryptoCurrency baseado na estratégia de investimento selecionada.
+     * @param investmentStrategy Estratégia de investimento da qual será sugerido uma CryptoCurrency.
+     * @return {@code CryptoCurrency} Moeda sugerida.
+     */
     public static CryptoCurrency getRandomCrypto(InvestmentStrategy investmentStrategy) {
         List<CryptoCurrency> cryptos = investmentStrategy.getSuggestedCryptos();
         Random rng = new Random();
@@ -37,7 +49,11 @@ public class InvestmentStrategyService {
         return cryptos.get(rng.nextInt(cryptos.size()));
     }
 
-    public static InvestmentStrategy getInvestmentStrategyByName(String strategyName) throws IOException {
+    /** Obtém uma instância da classe InvestmentStrategy pelo nome da estratégia.
+     * @param strategyName Nome da estratégia.
+     * @return {@code InvestmentStrategy} Instância de uma estratégia de investimento com nome identificado.
+     */
+    public static InvestmentStrategy getInvestmentStrategyByName(String strategyName) {
          switch (strategyName) {
             case "Conservative": return new ConservativeStrategy();
             case "Aggressive": return new AggressiveStrategy();
