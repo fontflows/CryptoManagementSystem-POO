014d326cd33c3306a656b3da998cdbff21851adb
alvaro_rc <alvarocampioni7@gmail.com>
2024-12-03 14:09:20 -0300

    java doc LoginRepository

--- src/main/java/com/cryptomanager/repositories/LoginRepository.java
+++ src/main/java/com/cryptomanager/repositories/LoginRepository.java
@@ -4,11 +4,17 @@ import org.springframework.stereotype.Repository;
 
 import java.io.*;
 
+/**Classe responsavel por lidar com a pertinencia de dados das informacoes do usuario logado*/
 @Repository
 public class LoginRepository {
 
     public static final String FILE_PATH = "loggedClient.txt";
 
+    /** Metodo responsavel por armazenar os dados do usuario logado.
+     * @param userID Recebe o userID do usuario logado.
+     * @param portfolioID Recebe o portfolioID do usuario logado.
+     * @throws IOException Caso ocorra algum erro de entrada/saida na manipulacao do arquivo.
+     */
     public void saveLoggedInfo(String userID, String portfolioID) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
             writer.write(userID + "," + portfolioID);
@@ -17,6 +23,10 @@ public class LoginRepository {
         }
     }
 
+    /** Metodo responsavel por ler os dados do usuario logado.
+     * @return Retorna um array de String com dois elementos, contendo o userID e portfolioID respectivamente.
+     * @throws IOException Caso ocorra algum erro de entrada/saida na manipulacao do arquivo.
+     */
     public String[] loadLoggedInfo() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
