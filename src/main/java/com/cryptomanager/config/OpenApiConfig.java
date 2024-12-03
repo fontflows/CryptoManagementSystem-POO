d2ca3a989fadaa36051e6dae427dc7d7da8e2598
alvaro_rc <alvarocampioni7@gmail.com>
2024-12-03 17:47:38 -0300

    javadoc dos Config

--- src/main/java/com/cryptomanager/config/OpenApiConfig.java
+++ src/main/java/com/cryptomanager/config/OpenApiConfig.java
@@ -5,9 +5,13 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/** Classe responsavel pela configuracao do OpenAPI */
 @Configuration
 public class OpenApiConfig {
 
+    /** Metodo responsavel pela configuracao personalizada da API.
+     * @return Retorna uma instancia do OpenAPI configurada com as informacoees da API.
+     */
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
--- src/main/java/com/cryptomanager/config/SecurityConfig.java
+++ src/main/java/com/cryptomanager/config/SecurityConfig.java
@@ -10,25 +10,35 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
+/** Classe responsavel pela configuracao da seguranca da aplicacao utilizando Spring Security*/
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     private final ClientRepository clientRepository;
     private final LoginRepository loginRepository;
 
+    /** Construtor SecurityConfig
+     * @param clientRepository Instancia que permite a conexao com a classe que manipula os dados dos Portfolios no arquivo.
+     * @param loginRepository Instancia que permite a conexao com a classe que manipula os dados dos usuarios logados no arquivo.
+     */
     public SecurityConfig(ClientRepository clientRepository, LoginRepository loginRepository) {
         this.clientRepository = clientRepository;
         this.loginRepository = loginRepository;
     }
 
+    /** Metodo responsavel por configurar as regras de seguranca dos endpoints da API, definindo como usuarios podem acessar diferentes partes da aplicacao.
+     * @param http instancia da classe HttpSecurity, que faz parte da configuracao de segurança no Spring Security
+     * @return Retorna uma instancia de SecurityFilterChain com as configuracoes de seguranca definidas
+     * @throws Exception Se ocorrer um erro durante a configuracao de seguranca.
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login").permitAll()
-                        .requestMatchers("/Admin/**").hasRole("ADMIN")
-                        .requestMatchers("/1/**").hasAnyRole("CLIENT", "ADMIN")
+                        .requestMatchers("/register", "/login").permitAll() // Permite acesso livre nas paginas de login e cadastro
+                        .requestMatchers("/Admin/**").hasRole("ADMIN") // Restringe o acesso aos endpoints definidos em /Admin/
+                        .requestMatchers("/1/**").hasAnyRole("CLIENT", "ADMIN") // Permite acesso apenas para usuarios autorizados nos demais endpoints
                         .requestMatchers("/2/**").hasAnyRole("CLIENT", "ADMIN")
                         .requestMatchers("/3/**").hasAnyRole("CLIENT", "ADMIN")
                         .requestMatchers("/4/**").hasAnyRole("CLIENT", "ADMIN")
@@ -40,13 +50,16 @@ public class SecurityConfig {
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")  // Caminho para a pagina de autenticação personalizada
                         .defaultSuccessUrl("/swagger-ui/index.html", true)  // URL de redirecionamento após o login bem-sucedido
-                        .failureUrl("/login?error=login")
-                        .permitAll()  // Permitir acesso à página de login sem autenticação
+                        .failureUrl("/login?error=login") // URL de redirecionamento após o login mal-sucedido
+                        .permitAll()  // Permite acesso à página de login sem autenticação
                 );
 
         return http.build();
     }
 
+    /** Metodo responsavel por carregar informacoes de login dos usuarios para efetuar autenticacao.
+     * @return Retorna uma instancia de UserDetailsService com as informacoes de usuario armazenadas no sistema para comparar com as inseridas na pagina de login.
+     */
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailsService(clientRepository, loginRepository);
