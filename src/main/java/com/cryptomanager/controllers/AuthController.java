4dc920c6ca3e8c6ee1cbc8a7ca835007c171db26
alvaro_rc <alvarocampioni7@gmail.com>
2024-12-03 14:08:34 -0300

    java doc AuthController

--- src/main/java/com/cryptomanager/controllers/AuthController.java
+++ src/main/java/com/cryptomanager/controllers/AuthController.java
@@ -7,21 +7,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+/**Classe responsavel por manipular os metodos de autenticacao do sistema*/
 @Controller
 public class AuthController {
     private final ClientService clientService;
 
+    /** Construtor AuthController
+     * @param clientService Instancia utilizada para manipulacao dos dados dos clientes.
+     */
     @Autowired
     public AuthController(ClientService clientService) {
         this.clientService = clientService;
     }
 
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "userlogin";  // Nome do arquivo HTML (userlogin.html)
+    /** Metodo responsavel por referenciar a pagina de autenticacao personalizada ao sistema.
+     * @return Retorna o nome do arquivo da pagina de autenticacao personalizada
+     */
+    @GetMapping("/authentication")
+    public String showAuthenticationForm() {
+        return "userAuthentication";  // Nome do arquivo HTML (userAuthentication.html)
     }
 
+    /** Metodo responsavel por receber os dados de cadastro realizados atraves da pagina de autenticacao personalizada.
+     * @param username Recebe o nome de usuario utilizado como userID.
+     * @param portfolioID Recebe o ID do portfolio do usuario.
+     * @param password Recebe a senha do usuario.
+     * @param strategyName Recebe a estrategia de investimento do portfolio do usuario.
+     * @return Retorna a pagina que o usuario sera redirecionado depois de se cadastrar.
+     */
     @PostMapping("/register")
     public String registerUser(@RequestParam String username,
                                @RequestParam String portfolioID,
@@ -30,7 +43,7 @@ public class AuthController {
         try {
             clientService.addClient(username, portfolioID, password, strategyName, 0, "CLIENT");
         } catch (Exception e) {
-            return "redirect:/login?error=register";
+            return "redirect:/login?error=register"; // Redireciona para a página de login com um alerta após o cadastro falhar.
         }
 
         return "redirect:/login?successRegister"; // Redireciona para a página de login após o cadastro com sucesso
