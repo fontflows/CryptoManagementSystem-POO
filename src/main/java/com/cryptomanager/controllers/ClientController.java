dfa25aa314b52453582fe2a647aa3dee0265f574
alvaro_rc <alvarocampioni7@gmail.com>
2024-12-03 14:35:59 -0300

    Atualizacao ClientController

--- src/main/java/com/cryptomanager/controllers/ClientController.java
+++ src/main/java/com/cryptomanager/controllers/ClientController.java
@@ -1,7 +1,5 @@
 package com.cryptomanager.controllers;
 
-
-import com.cryptomanager.models.StrategyNames;
 import com.cryptomanager.exceptions.ClientServiceException;
 import com.cryptomanager.repositories.LoginRepository;
 import com.cryptomanager.services.ClientService;
@@ -11,20 +9,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
+/**Classe responsavel por facilitar a checagem e manipulacao de dados dos usuarios no sistema.*/
 @RestController
 @RequestMapping("1/client-info")
-
- /**Classe responsavel por facilitar a checagem e manipulacao de dados dos usuarios no sistema Swagger*/
 public class ClientController{
     private final ClientService clientService;
     private final LoginRepository loginRepository;
 
+    /** Construtor ClientController
+     * @param clientService Instancia utilizada para manipulacao dos dados dos clientes.
+     * @param loginRepository Instancia que conecta o Controller com a classe que manipula os dados dos usuarios logados no arquivo.
+     */
     @Autowired
     public ClientController(ClientService clientService, LoginRepository loginRepository) {
         this.clientService = clientService;
         this.loginRepository = loginRepository;
     }
 
+    /** Metodo responsavel por obter as informacoes do usuario logado.
+     * @return Retorna as informacoes do usuario logado.
+     */
     @GetMapping("/get-own-info")
     public ResponseEntity<?> getLoggedClient() {
         try {
@@ -34,6 +38,10 @@ public class ClientController{
         }
     }
 
+    /** Metodo reponsavel por permitir um usuario alterar sua propria senha.
+     * @param newPassword Recebe a nova senha do usuario.
+     * @return Mensagem de retorno da correta execucao das funcoes associadas a edicao da senha do usuario.
+     */
     @PostMapping("/edit-own-password")
     public ResponseEntity<String> updateLoggedClientPassword(@RequestParam String newPassword){
         try {
