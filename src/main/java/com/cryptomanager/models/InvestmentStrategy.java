b83ba489411f152b1ef0db75b3989ae1d14e03ac
alvaro_rc <alvarocampioni7@gmail.com>
2024-12-04 09:00:06 -0300

    modificacoes necessarias
    metodos inutilizados

--- src/main/java/com/cryptomanager/models/InvestmentStrategy.java
+++ src/main/java/com/cryptomanager/models/InvestmentStrategy.java
@@ -6,9 +6,9 @@ import java.util.List;
 /** Classe modelo da estrutura padrao do nome da estrategia de investimentos de cada usuario do sistema.
  */
 public class InvestmentStrategy {
-    private String name;
-    private List<CryptoCurrency> suggestedCryptos = new ArrayList<>();
-    private int riskQuota;
+    private final String name;
+    private final List<CryptoCurrency> suggestedCryptos = new ArrayList<>();
+    private final int riskQuota;
 
     /** Construtor padrao da classe InvestmentStrategy.
      * @param name Recebe o nome do investimento.
@@ -19,13 +19,6 @@ public class InvestmentStrategy {
         this.riskQuota = riskQuota;
     }
 
-    /** Metodo responsavel por atribuir o nome da estrategia de investimento do usuario.
-     * @param name Recebe o nome do investimento.
-     */
-    public void setInvestmentStrategyName(String name){
-        this.name = name;
-    }
-
     /** Metodo responsavel por obter o nome da estrategia de investimento do usuario.
      * @return Retorna o nome da estrategia de investimento.
      */
@@ -33,25 +26,11 @@ public class InvestmentStrategy {
         return name;
     }
 
-    /** Metodo responsavel por atribuir a cota de risco da estrategia de investimento do usuario.
-     * @param riskQuota Recebe o valor da cota de risco da estrategia de investimento.
-     */
-    public void setRiskQuota(int riskQuota){
-        this.riskQuota = riskQuota;
-    }
-
     /** Metodo responsavel por obter a cota de risco da estrategia de investimento.
      * @return Retorna a cota de risco da estrategia de investimento.
      */
     public int getRiskQuota(){
             return riskQuota;
-    }
-
-    /** Metodo responsavel por atribuir as criptomoedas sugeridas para a estrategia de investimento informada pelo usuario no sistema.
-     * @param suggestedCryptos Recebe a lista de criptomoedas adequadas/apropriadas para a estrategia de investimento declarada.
-     */
-    public void setSuggestedCryptos(List<CryptoCurrency> suggestedCryptos){
-        this.suggestedCryptos = suggestedCryptos;
     }
 
     /** Metodo responsavel por obter as criptomoedas sugeridas para a estrategia de investimento informada pelo usuario do sistema.
